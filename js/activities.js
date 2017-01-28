bc7a132173ca8ef01ac110955fd0cf65ce7a5ba8
mozilla <luke.pacholski@gmail.com>
2017-01-27 23:00:31 -0800

    some extra tags

--- js/activities.js
+++ js/activities.js
@@ -4,7 +4,7 @@ var activities = [
     thumbnail_url : "https://thimble.mozilla.org/img/thumbnail-keep-calm.png",
     url: "https://thimble.mozilla.org/en-US/projects/72/",
     description: "Change the design of this poster to make it your own.",
-    tags: ["css","html","color"],
+    tags: ["css","html","color","design"],
     featured : true,
     teaching_kit_url: "https://thimbleprojects.org/mozillalearning/171/"
   },
@@ -13,7 +13,7 @@ var activities = [
     thumbnail_url : "https://thimble.mozilla.org/img/thumbnail-postcard.png",
     url: "https://thimble.mozilla.org/en-US/projects/72/",
     description: "Add your own content to this interactive postcard.",
-    tags: ["images","code","javascript","interactive"],
+    tags: ["images","code","javascript","interactive","js"],
     teaching_kit_url: "https://thimbleprojects.org/mozillalearning/62/",
     featured : true
   },
@@ -22,7 +22,7 @@ var activities = [
     thumbnail_url : "https://thimble.mozilla.org/img/thumbnail-comic-strip.png",
     url: "https://thimble.mozilla.org/en-US/projects/72/",
     description: "Create your very own version of this comic strip.",
-    tags: ["fun","images","attribution","search"],
+    tags: ["fun","images","attribution","search","img"],
     featured : true,
     teaching_kit_url: "https://thimbleprojects.org/mozillalearning/171/"
   },
@@ -31,7 +31,7 @@ var activities = [
     thumbnail_url : "https://thimble.mozilla.org/img/thumbnail-three-things-i-heart.png",
     url: "https://thimble.mozilla.org/en-US/projects/72/",
     description: "Make a simple website to share what you love.",
-    tags: ["images","links"],
+    tags: ["images","links","story","creative"],
     teaching_kit_url: "https://thimbleprojects.org/mozillalearning/11288/"
   },
   {
@@ -39,7 +39,7 @@ var activities = [
     thumbnail_url : "https://thimble.mozilla.org/img/thumbnail-homework-excuse-generator.png",
     url: "https://thimble.mozilla.org/en-US/projects/72/",
     description: "Remix a basic JavaScript function to explain why your homework is late.",
-    tags: ["code","array","javascript"],
+    tags: ["code","array","javascript","js"],
     teaching_kit_url: "https://thimbleprojects.org/mozillalearning/11701/"
   },
   {
@@ -47,7 +47,7 @@ var activities = [
     thumbnail_url : "https://thimble.mozilla.org/img/thumbnail-my-six-word-summer.png",
     url: "https://thimble.mozilla.org/en-US/projects/72/",
     description: "Tell us what you did last summer in just six words.",
-    tags: ["css","html","writing"],
+    tags: ["html","writing","narrative"],
     teaching_kit_url: "https://thimbleprojects.org/mozillalearning/11704/"
   }
 ];
